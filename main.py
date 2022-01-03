3bc5595db326b05c67b9e3c71adead38c671a1c7
diduk228 <danil.glebov.1999@mail.ru>
2022-01-03 20:10:22 +0700

    Добавил класс Kopilka_reg

--- main.py
+++ main.py
@@ -83,217 +83,247 @@ def get_inf_numbers_api():
     print(req.text)
 
 
-def get_person():
-    file1 = open("persons.txt", "r")
-    len = file1.readline()
-    count = 0
-    name = ''
-    famaly_name = ''
-    otc = ''
-    find = random.randint(0, int(len)-1)
-    while True:
-        if(count == find):
-            line = file1.readline()
-            if not line:
-                break
-            famaly_name = line.rpartition(' ')[0]
-            famaly_name = famaly_name.rpartition(' ')[0]
-            name = line.partition(' ')[2]
-            name = name.rpartition(' ')[0]
-            otc = line.partition(' ')[2]
-            otc = otc.partition(' ')[2]
-            count = count + 1
-            break
-        temp = file1.readline()
-        if not temp:
-            break
-        count = count + 1
-    file1.close
-    return famaly_name, name, otc
-
-def get_key():
-    file1 = open("keys.txt", "r")
-    count = 0
-    find = random.randint(1, 198)
-    key = ''
-    while True:
-        if(count == find):
-            line = file1.readline()
-            key = line
-            if not line:
-                break
-            count = count + 1
-            break
-        temp = file1.readline()
-        if not temp:
-            break
-        count = count + 1
-    file1.close
-    if(len(key) > 8):
-        key = get_key()
-    return key
-def get_area():
-    k = random.randint(1, 4)
-    print(k)
-    if(k == 1):
-        area = 'Иркутская Область\n'
-        city = 'Ангарск'
-        return area, city
-    if(k == 2):
-        area = 'Красноярский Край\n'
-        city = 'Минусинск'
-        return area, city
-    if(k == 3):
-        area = 'Красноярский Край\n'
-        city = 'Канск'
-        return area, city
-    if(k == 4):
-        area = 'Красноярский Край\n'
-        city = 'Красноярск'
-        return area, city
-def push_next(driver):
-    next_btn = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/form/div[6]/div/div[8]/span/input')
-    next_btn.click()
-def get_card():
-    file1 = open("cards.txt", "r")
-    line_get = file1.readline()
-    file1.close()
-    file1 = open("cards.txt", "r")
-    lines = file1.readlines()
-    file1.close()
-
-    file1 = open("cards.txt", "w")
-    pattern = re.compile(re.escape(line_get))
-    for line in lines:
-        result = pattern.search(line)
-        if result is None:
-            file1.write(line)
-    file1.close()
-    return line_get
 def save_acc(number, passw):
     file1 = open("new_acc.txt", "a")  # режим добавления
     file1.write(number + ":" + passw + '\n')
     file1.close()
-def reg(rep = 0, number = '', id = '', card = 0):
-    famaly_name = ''
-    name = ''
-    otc = ''
-    famaly_name1, name1, otc1 = get_person()
-    famaly_name = famaly_name1
-    famaly_name1, name1, otc1 = get_person()
-    name = name1
-    famaly_name1, name1, otc1 = get_person()
-    otc = otc1
-    key = get_key()
-    ser = Service("/home/danil/PycharmProjects/Koplka/geckodriver")
-    op = webdriver.FirefoxOptions()
-    driver = webdriver.Firefox(executable_path="/home/danil/PycharmProjects/Koplka/geckodriver")
-    driver.get('https://kopilkaclub.ru/register/card')
-    last_name_btn = driver.find_element_by_xpath("//input[@name='inp[ln]']")
-    last_name_btn.send_keys(famaly_name)
-    name_btn = driver.find_element_by_xpath("//input[@name='inp[fn]']")
-    name_btn.send_keys(name)
-    otces_btn = driver.find_element_by_xpath("//input[@name='inp[mn]']")
-    otces_btn.send_keys(otc)
-    data_btn = driver.find_element_by_xpath("//input[@name='inp[dob]']")
-    dates = '0601' + str(random.randint(1987, 2004))
-    data_btn.send_keys(dates)
-    if(rep == 0):
-        number, id = buy_number()
-    for_text = number # Дальше буду использовать этот вариант для поиска ошибок
-    print(number + ' ' + id)
-    number = number[2:]
-    print(number + ' ' + id)
-    phone_btn = driver.find_element_by_xpath("//input[@name='inp[phone]']")
-    phone_btn.send_keys(number)
-    # email_btn = driver.find_element_by_xpath("//input[@name='inp[email]']")
-    # email_btn.send_keys('homefg@mail.ru')
-    card_btn = driver.find_element_by_xpath("//input[@name='inp[card]']")
-    if(card == 0):
-        card_btn.send_keys(get_card())
-    else:
+
+class Kopilka_reg:
+    def __init__(self):
+        self.driver = webdriver.Firefox(executable_path="/home/danil/PycharmProjects/Koplka/geckodriver")
+
+    def get_person(self):
+        file1 = open("persons.txt", "r")
+        len = file1.readline()
+        count = 0
+        name = ''
+        famaly_name = ''
+        otc = ''
+        find = random.randint(0, int(len) - 1)
+        while True:
+            if (count == find):
+                line = file1.readline()
+                if not line:
+                    break
+                famaly_name = line.rpartition(' ')[0]
+                famaly_name = famaly_name.rpartition(' ')[0]
+                name = line.partition(' ')[2]
+                name = name.rpartition(' ')[0]
+                otc = line.partition(' ')[2]
+                otc = otc.partition(' ')[2]
+                count = count + 1
+                break
+            temp = file1.readline()
+            if not temp:
+                break
+            count = count + 1
+        file1.close
+        return famaly_name, name, otc
+
+    def get_key(self):
+        file1 = open("keys.txt", "r")
+        count = 0
+        find = random.randint(1, 198)
+        key = ''
+        while True:
+            if (count == find):
+                line = file1.readline()
+                key = line
+                if not line:
+                    break
+                count = count + 1
+                break
+            temp = file1.readline()
+            if not temp:
+                break
+            count = count + 1
+        file1.close
+        if (len(key) > 8):
+            key = self.get_key()
+        return key
+
+    def get_area(self):
+        k = random.randint(1, 4)
+        print(k)
+        if (k == 1):
+            area = 'Иркутская Область\n'
+            city = 'Ангарск'
+            return area, city
+        if (k == 2):
+            area = 'Красноярский Край\n'
+            city = 'Минусинск'
+            return area, city
+        if (k == 3):
+            area = 'Красноярский Край\n'
+            city = 'Канск'
+            return area, city
+        if (k == 4):
+            area = 'Красноярский Край\n'
+            city = 'Красноярск'
+            return area, city
+
+    def last_name_btn(self, famaly_name):
+        last_name_btn = self.driver.find_element_by_xpath("//input[@name='inp[ln]']")
+        last_name_btn.send_keys(famaly_name)
+    def name_btn(self, name):
+        name_btn = self.driver.find_element_by_xpath("//input[@name='inp[fn]']")
+        name_btn.send_keys(name)
+    def otces_btn(self, otc):
+        otces_btn = self.driver.find_element_by_xpath("//input[@name='inp[mn]']")
+        otces_btn.send_keys(otc)
+    def data_btn(self):
+        data_btn = self.driver.find_element_by_xpath("//input[@name='inp[dob]']")
+        dates = '0701' + str(random.randint(1987, 2004))
+        data_btn.send_keys(dates)
+    def phone_btn(self,number):
+        phone_btn = self.driver.find_element_by_xpath("//input[@name='inp[phone]']")
+        phone_btn.send_keys(number)
+
+    def get_card(self):
+        file1 = open("cards.txt", "r")
+        line_get = file1.readline()
+        file1.close()
+        file1 = open("cards.txt", "r")
+        lines = file1.readlines()
+        file1.close()
+
+        file1 = open("cards.txt", "w")
+        pattern = re.compile(re.escape(line_get))
+        for line in lines:
+            result = pattern.search(line)
+            if result is None:
+                file1.write(line)
+        file1.close()
+        return line_get
+    def card_btn(self, card):
+        card_btn = self.driver.find_element_by_xpath("//input[@name='inp[card]']")
         card_btn.send_keys(card)
-    #     2282696 2282700 2282701 2282702 2282703 2282705 2282707
-    secret_btn = driver.find_element_by_xpath("//input[@name='inp[secret]']")
-    secret_btn.send_keys(key)
-    iwant_sms_btn = driver.find_element_by_xpath("//input[@id='inp[iwant_sms]']")
-    iwant_sms_btn.click()
-    iwant_email_btn = driver.find_element_by_xpath("//input[@id='inp[iwant_email]']")
-    iwant_email_btn.click()
-    next_btn = driver.find_element_by_xpath("//input[@class='btn btn-success btn-lg next-step']");
-    next_btn.click()
-    area, city = get_area()
-    oblast_btn = driver.find_element_by_xpath("//span[@id='select2-inpregion-container']")
-    oblast_btn.click()
-    oblast_btn = driver.find_element_by_xpath("//input[@class='select2-search__field']")
-    oblast_btn.send_keys(area)
+    def secret_btn(self, key):
+        secret_btn = self.driver.find_element_by_xpath("//input[@name='inp[secret]']")
+        secret_btn.send_keys(key)
+    def iwant_sms_btn(self):
+        iwant_sms_btn = self.driver.find_element_by_xpath("//input[@id='inp[iwant_sms]']")
+        iwant_sms_btn.click()
+    def iwant_email_btn(self):
+        iwant_email_btn = self.driver.find_element_by_xpath("//input[@id='inp[iwant_email]']")
+        iwant_email_btn.click()
+    def next_1_btn(self):
+        next_btn = self.driver.find_element_by_xpath("//input[@class='btn btn-success btn-lg next-step']");
+        next_btn.click()
+    def area_btn(self, area):
+        oblast_btn = self.driver.find_element_by_xpath("//span[@id='select2-inpregion-container']")
+        oblast_btn.click()
+        oblast_btn = self.driver.find_element_by_xpath("//input[@class='select2-search__field']")
+        oblast_btn.send_keys(area)
+    def city_btn(self, city):
+        gorod_btn = self.driver.find_element_by_xpath("//span[@id='select2-inpcity-container']")
+        gorod_btn.click()
+        gorod_btn = self.driver.find_element_by_xpath("//input[@class='select2-search__field']")
+        gorod_btn.send_keys(city)
+        time.sleep(3)
+        gorod_btn.send_keys(city + '\n')
+    def street_btn(self, number, id):
+        uliza_btn = self.driver.find_element_by_xpath("//span[@id='select2-inpstreet-container']")
+        uliza_btn.click()
+        try:
+            uliza_btn = self.driver.find_element_by_xpath("//input[@class='select2-search__field']")
+            alf = 'абвгдежзиклмнопрстуфхцчшщэюя'
+            bukv = alf[random.randint(0, len(alf) - 1)]
+            uliza_btn.send_keys(bukv)
+            time.sleep(4)
+            uliza_btn.send_keys(bukv + '\n')
+        except:
+            self.reg(rep=1, number=number, id=id)
 
-    gorod_btn = driver.find_element_by_xpath("//span[@id='select2-inpcity-container']")
-    gorod_btn.click()
-    gorod_btn = driver.find_element_by_xpath("//input[@class='select2-search__field']")
-    gorod_btn.send_keys(city)
-    time.sleep(3)
-    gorod_btn.send_keys(city + '\n')
+    def house_btn(self):
+        dom_btn = self.driver.find_element_by_xpath("//input[@id='inp[house]']")
+        dom_btn.send_keys(str(random.randint(1, 99)))
+    def next_2_btn(self):
+        next_btn = self.driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/form/div[6]/div/div[8]/span/input')
+        next_btn.click()
+    def next_3_btn(self):
+        next_btn = self.driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/form/div[5]/div/input')
+    def confirm_cod_btn(self,cod):
+        finil = self.driver.find_element_by_xpath("//*[@id='confirmCode']")
+        finil.send_keys(cod)
 
-    uliza_btn = driver.find_element_by_xpath("//span[@id='select2-inpstreet-container']")
-    uliza_btn.click()
-    try:
-        uliza_btn = driver.find_element_by_xpath("//input[@class='select2-search__field']")
-        alf = 'абвгдежзиклмнопрстуфхцчшщэюя'
-        bukv = alf[random.randint(0, len(alf)-1)]
-        uliza_btn.send_keys(bukv)
-        time.sleep(4)
-        uliza_btn.send_keys(bukv + '\n')
-    except:
-        reg(rep=1, number=number, id=id)
 
-    dom_btn = driver.find_element_by_xpath("//input[@id='inp[house]']")
-    dom_btn.send_keys(str(random.randint(1, 99)))
-
-    time.sleep(3)
-    try:
-        push_next(driver) #Кнопка далее(вторая)
-    except:
-        time(4)
-        push_next(driver)
-    try:
-        mistake = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/form/div[4]/p') #текс что нужно вести код подтверждения
-        #/html/body/div[3]/div/div/div[1]/div/form/div[4]/p[3]
-
-        print('Ошибка регистрации')
-        #reg(rep=0, number=number, id=id)
-    except:
-        print('')
-    try:
-        bad_card = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/form/div[4]/p/span') # Ошибка если карта уже используется
-        if(bad_card.text == 'номер карты :'):
-            driver.quit()
-            reg(rep=1,number=for_text, id=id,card=0)
+    def reg(self, rep = 0, number = '', id = '', card = 0):
+        famaly_name1, name1, otc1 = self.get_person()
+        famaly_name = famaly_name1
+        famaly_name1, name1, otc1 = self.get_person()
+        name = name1
+        famaly_name1, name1, otc1 = self.get_person()
+        otc = otc1
+        key = self.get_key()
+        self.driver.get('https://kopilkaclub.ru/register/card')
+        self.last_name_btn(famaly_name=famaly_name)
+        self.name_btn(name=name)
+        self.otces_btn(otc=otc)
+        self.data_btn()
+        if (rep == 0):
+            number, id = buy_number()
+        for_text = number  # Дальше буду использовать этот вариант для поиска ошибок
+        print(number + ' ' + id)
+        number = number[2:]
+        print(number + ' ' + id)
+        self.phone_btn(number=number)
+        if (card == 0):
+            self.card_btn(card=self.get_card())
         else:
+            self.card_btn(card=card)
+        self.secret_btn(key)
+        self.iwant_sms_btn()
+        self.iwant_email_btn()
+        self.next_1_btn()
+        area, city = self.get_area()
+        self.area_btn(area=area)
+        self.city_btn(city=city)
+        self.street_btn(number=for_text,id=id)
+        self.house_btn()
+        time.sleep(3)
+        self.next_2_btn()
+        time.sleep(4)
+        try:
+            mistake = self.driver.find_element_by_xpath( '/html/body/div[3]/div/div/div[1]/div/form/div[4]/p')  # текс что нужно вести код подтверждения
+            # /html/body/div[3]/div/div/div[1]/div/form/div[4]/p[3]
+
+            print('Ошибка регистрации')
+            # reg(rep=0, number=number, id=id)
+        except:
+            print('')
+        try:
+            bad_card = self.driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/form/div[4]/p/span')  # Ошибка если карта уже используется
+            if (bad_card.text == 'номер карты :'):
+                print('Номер карты не уникальный')
+                self.driver.quit()
+                self.reg(rep=1, number=for_text, id=id, card=0)
+            else:
+                print('Номер карты уникальный')
+        except:
             print('Номер карты уникальный')
-    except:
-        print('Номер карты уникальный')
-    try:
-        bad_phone = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/form/div[4]/p/span') # Ошибка если номер уже используется
-        if (bad_phone.text == 'телефон : '):
-            driver.quit()
-            reg(rep=0, card=card)
-    except:
-        print('Номер телефона уникальный')
-    try:
-        next_btn = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/form/div[5]/div/input')
-    except:
-        reg(rep=1, number=for_text, id=id)
-    cod, passwor = get_sms(id, mod=0)
-    finil = driver.find_element_by_xpath("//*[@id='confirmCode']")
-    finil.send_keys(cod)
-    next_btn.click()
-    cod, passwor = get_sms(id, mod=1)
-    save_acc(number, passwor)
-    driver.quit()
-    reg()
-    # try:
-    #     succses = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div/form/div[3]/div/p[1]')
+        try:
+            bad_phone = self.driver.find_element_by_xpath( '/html/body/div[3]/div/div/div[1]/div/form/div[4]/p/span')  # Ошибка если номер уже используется
+            print('Номер телефона не уникальный')
+            if (bad_phone.text == 'телефон : '):
+                self.driver.quit()
+                self.reg(rep=0, card=card)
+        except:
+            print('Номер телефона уникальный')
+
+        cod, passwor = get_sms(id, mod=0)
+        self.confirm_cod_btn(cod=cod)
+        self.next_3_btn()
+        cod, passwor = get_sms(id, mod=1)
+        save_acc(number, passwor)
+        self.driver.quit()
+        self.reg()
+
+
+
+
+
+
 
 class Kopilka_aut:
     def __init__(self):
@@ -360,11 +390,13 @@ class Kopilka_aut:
             self.get_balance(number=number)
             self.driver.delete_all_cookies()
 
+
 print('If you want to cheak accaunts, push 1\nIf you want to reg accaunt, push 0 ')
 mod = int(input())
 if(mod == 1):
     start = Kopilka_aut()
     start.aut()
 if(mod == 0):
-    reg()
+    start = Kopilka_reg()
+    start.reg()
 
